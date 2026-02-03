c1f8ef22d56c1e73ce9cebb5b8769444b3ce56c5
Mariam982440 <mariam1elhaimer@gmail.com>
2026-02-03 15:17:33 +0100

    feat: création des migrations categories dishes photos et favorites

---
+++ database/migrations/2026_02_03_134950_create_dishes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->decimal('price', 8, 2); // Float dans votre diagramme
+            $table->string('photo')->nullable();
+            $table->boolean('is_available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
