618e899e3456bcfd40a9977ed2e7660bc1c96d13
Mariam982440 <mariam1elhaimer@gmail.com>
2026-02-12 14:53:57 +0100

    feat : création du model de la reservation avec ses relations

---
+++ app/Models/Reservation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    protected $fillable=["user_id","restaurant_id","reservation_date","slot","guests_count","status"];
+
+    public function user()
+    { 
+        return $this->belongsTo(User::class); 
+        } 
+    public function restaurant() 
+    { 
+        return $this->belongsTo(Restaurant::class); 
+    }
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
+}
