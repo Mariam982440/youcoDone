ecdacd705389dea6c513f88e649d5a41b08a7747
Mariam982440 <mariam1elhaimer@gmail.com>
2026-02-12 14:57:20 +0100

    feat : création du model payment avec ses relations

---
+++ app/Models/Payment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $fillable=["reservation_id","amount","payment_method","status","transaction_id","currency"];
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+    
+}
